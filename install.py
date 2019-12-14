987f358702597f3e3706855bfaf871e027cfae93
AntoinePrv <AntoinePrv@users.noreply.github.com>
2019-12-14 17:20:11 -0500

    Add editrc

--- install.py
+++ install.py
@@ -203,6 +203,7 @@ def main() -> None:
         FilesInstall(source=PROJECT_DIR/"base16", dest=DATA_DIR/"base16"),
         FilesInstall(source=PROJECT_DIR/"misc", dest=CONFIG_DIR/"misc"),
         FilesInstall(source=CONFIG_DIR/"misc/inputrc", dest=HOME_DIR/".inputrc"),
+        FilesInstall(source=CONFIG_DIR/"misc/editrc", dest=HOME_DIR/".editrc"),
         UpdateNvimPackages(),
         NvimGeneratePromptline(
             script=PROJECT_DIR/"bash/default-prompt.vim",
