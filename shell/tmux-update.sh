ee92e55bf06da6fbbb723008e13445acbbf66e73
AntoinePrv <AntoinePrv@users.noreply.github.com>
2020-12-28 18:40:59 +0100

    Make update-tmux shell agnostic

--- shell/tmux-update.sh
+++ shell/tmux-update.sh
@@ -1,12 +1,14 @@
 # Pane local environment variable in tmux environment.
 # Update the value of an environment variable from the current pane to a
 # pane-local variable in tmux environment.
-# The pane-local varaible is emulated using the pane-id.
+# The pane-local variable is emulated using the pane-id.
 function __update_local_env () {
-	if [ -z "${!1}" ]; then
-		tmux setenv -u "__TMUX${TMUX_PANE}_$1"
+	local -r var_name="$1"
+	local -r var_val="$(eval "printf %s \$$var_name")"
+	if [ -z "${var_val}" ]; then
+		tmux setenv -u "__TMUX${TMUX_PANE}_${var_name}"
 	else
-		tmux setenv "__TMUX${TMUX_PANE}_$1" "${!1}"
+		tmux setenv "__TMUX${TMUX_PANE}_${var_name}" "${var_val}"
 	fi
 }
 
@@ -20,7 +22,7 @@ function __update_tmux () {
 	tmux refresh-client -S
 }
 
-# Upadte PROMPT_COMMAND
+# Update PROMPT_COMMAND
 if [[ ! "$PROMPT_COMMAND" == *__update_tmux* ]]; then
 	export PROMPT_COMMAND+='__update_tmux;'
 fi
