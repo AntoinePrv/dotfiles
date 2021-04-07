244cc965641dd0cdd6abe6e64c0c944bd2afa96d
AntoinePrv <AntoinePrv@users.noreply.github.com>
2021-04-07 13:13:15 -0400

    Zinit add bat, fd, fzf, etc.

--- shell/zinit.zsh
+++ shell/zinit.zsh
@@ -9,8 +9,28 @@ function initialize_zinit {
 	source "${dest}/zinit.zsh"
 }
 
-# Setupt path and initalize zinit
+# Setup path and initalize zinit
 declare -A ZINIT
 ZINIT[HOME_DIR]="${XDG_DATA_HOME}/zinit"
 ZINIT[BIN_DIR]="${ZINIT[HOME_DIR]}/zinit"
 initialize_zinit "3.7" "${ZINIT[BIN_DIR]}"
+
+# Provides sbin etc. ice for managing programs as shims
+zinit ice compile
+zinit light @zinit-zsh/z-a-bin-gem-node
+
+zinit ice lucid from='gh-r' mv='rip* ripgrep' sbin='**/rg(.exe|) -> rg'
+zinit light @BurntSushi/ripgrep
+
+zinit ice lucid from='gh-r' mv='fd* fd' sbin='**/fd(.exe|) -> fd'
+zinit light @sharkdp/fd
+
+zinit ice lucid from='gh-r' mv='bat* bat' sbin='**/bat(.exe|) -> bat'
+zinit light @sharkdp/bat
+
+zinit ice wait compile lucid blockf
+zinit light @zsh-users/zsh-completions
+
+# Should be loaded last and call again compinit after all (turbo) completion are loaded
+zinit ice wait compile lucid atinit='ZINIT[COMPINIT_OPTS]=-C; zicompinit; zicdreplay'
+zinit light zdharma/fast-syntax-highlighting
