f1635ec2e92bbe7eb379fba4f4bcc8c4dfee1db7
AntoinePrv <AntoinePrv@users.noreply.github.com>
2021-04-15 12:03:19 -0400

    Improve fswatch monitoring in Ecole

--- tmuxp/ecole/watch.sh
+++ tmuxp/ecole/watch.sh
@@ -35,7 +35,7 @@ function __title () {
 }
 
 
-cancel_and_run() {
+function cancel_and_run () {
 	local command_pid=0
 
 	while read; do
@@ -49,44 +49,48 @@ cancel_and_run() {
 	done
 }
 
+function __fswatch () {
+	fswatch --latency=1 --monitor=poll_monitor --one-per-batch --recursive "$@"
+}
+
 function __build () {
-	clear && __title Build && cmake --build build/ --parallel -- -s
+	clear && __title Build && cmake --build build/ --parallel "$@" -- -s
 }
 
 function __watch_build () {
-	fswatch -o CMakeLists.txt libecole/ python/ | cancel_and_run __build
+	__fswatch CMakeLists.txt libecole/ python/ cmake/ | cancel_and_run __build "$@"
 }
 
 function __doc () {
-	clear && __title Generate Doc && cmake --build build/ --target ecole-sphinx -- -s
+	clear && __title Generate Doc && cmake --build build/ --target ecole-sphinx "$@" -- -s
 }
 
 function __watch_doc () {
-	fswatch -o CMakeLists.txt docs/ libecole/ python/ | cancel_and_run __doc
+	__fswatch CMakeLists.txt docs/ libecole/ python/ | cancel_and_run __doc "$@"
 }
 
 function __tests () {
-	clear && __title Tests && ./build/libecole/tests/test-libecole
+	clear && __title Tests && ./build/libecole/tests/test-libecole "$@"
 }
 
 function __watch_tests () {
-	fswatch -o build/libecole/tests/test-libecole | cancel_and_run __tests
+	__fswatch build/libecole/tests/test-libecole | cancel_and_run __tests "$@"
 }
 
 function __pytest () {
 	(
 		clear && __title PyTest && \
-		./build/venv/bin/python -u -m pytest --no-slow -o cache_dir="${XDG_CACHE_HOME}/pytest/ecole" python/
+		./build/venv/bin/python -u -m pytest --no-slow -o cache_dir="${XDG_CACHE_HOME}/pytest/ecole" "$@" python/
 	)
 }
 
 function __watch_pytest () {
 	WATCH_PATHS="build/venv python/tests python/**/*.py"
-	fswatch --exclude '.*\.pyc' -o -r ${WATCH_PATHS} --latency=1 | cancel_and_run __pytest
+	__fswatch --exclude '.*\.pyc' ${WATCH_PATHS} | cancel_and_run __pytest "$@"
 }
 
 function __main() {
-	"__watch_${1}"
+	"__watch_${1}" "${@:2}"
 }
 
 __main $@
