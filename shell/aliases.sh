aea74b54e1e2d1eda25b478ca5ad7126ef7f235f
AntoinePrv <AntoinePrv@users.noreply.github.com>
2021-05-04 09:51:21 -0400

    Add docker-shell alias

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -9,3 +9,9 @@ alias grep='grep --color=auto'
 type bat &> /dev/null && alias cat='bat'
 type nvim &> /dev/null && alias vim='nvim'
 type python3 &> /dev/null && alias python='python3'
+
+function docker-shell () {
+	docker run \
+		--interactive --tty --rm \
+		--workdir "/workspace" --mount "type=bind,source=${PWD},target=/workspace/$(basename ${PWD})" "$@"
+}
