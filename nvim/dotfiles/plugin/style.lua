be9ee344747e52c296e2309c354154e2884014fb
AntoinePrv <AntoinePrv@users.noreply.github.com>
2022-08-10 09:33:50 +0200

    Change file to lua

---
+++ nvim/dotfiles/plugin/style.lua
@@ -0,0 +1,25 @@
+-- Line numbers
+vim.opt.number = true
+
+-- Activate mouse
+vim.opt.mouse = "a"
+
+-- Hidden characters
+vim.opt.list = true
+vim.opt.listchars = {eol="¬", tab="──→", trail="·", nbsp="⌴", extends="…", precedes="…"}
+
+-- Soft wrap lines with indent
+vim.opt.wrap = true
+vim.opt.breakindent = true
+vim.opt.showbreak = "↳"
+
+-- Show vertical rule at column
+vim.opt.colorcolumn = "100"
+
+-- Show command at the bottom right
+vim.opt.showcmd = true
+
+-- Default folding level when opening new buffer
+vim.opt.foldlevelstart = 4
+
+vim.cmd([[highlight EndOfBuffer ctermfg=bg guifg=bg]])
