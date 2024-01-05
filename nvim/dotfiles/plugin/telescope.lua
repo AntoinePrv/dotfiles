db474b2c106d01498239bcfccaebd41eb1a3f493
AntoinePrv <AntoinePrv@users.noreply.github.com>
2024-01-05 17:54:41 +0100

    Fix Telescope goto errors

--- nvim/dotfiles/plugin/telescope.lua
+++ nvim/dotfiles/plugin/telescope.lua
@@ -42,21 +42,43 @@ function alternate_files()
 end
 
 vim.keymap.set("n", "gf", function() telescope_builtin.git_files({show_untracked=true}) end, {})
+vim.keymap.set("n", "g*", telescope_builtin.grep_string, {})
+vim.keymap.set("n", "g/", telescope_builtin.current_buffer_fuzzy_find, {})
+vim.keymap.set("n", "gg", telescope_builtin.live_grep, {})
+vim.keymap.set("n", "gb", telescope_builtin.buffers, {})
+vim.keymap.set("n", "gh", telescope_builtin.help_tags, {})
+vim.keymap.set("n", "gm", telescope_builtin.marks, {})
+vim.keymap.set("n", "gc", telescope_builtin.quickfix, {})
 vim.keymap.set("n", "gd", telescope_builtin.lsp_definitions, {})
 vim.keymap.set("n", "gt", telescope_builtin.lsp_type_definitions, {})
 vim.keymap.set("n", "gr", telescope_builtin.lsp_references, {})
 vim.keymap.set("n", "gs", telescope_builtin.lsp_document_symbols, {})
 vim.keymap.set("n", "gS", telescope_builtin.lsp_workspace_symbols, {})
-vim.keymap.set("n", "ge", function() telescope_builtin.diagnostics({bufnr=0}) end, {})
-vim.keymap.set("n", "gE", telescope_builtin.diagnostics, {})
-vim.keymap.set("n", "g*", telescope_builtin.grep_string, {})
-vim.keymap.set("n", "g/", telescope_builtin.current_buffer_fuzzy_find, {})
-vim.keymap.set("n", "gg", telescope_builtin.live_grep, {})
-vim.keymap.set("n", "gb", telescope_builtin.buffers, {})
+vim.keymap.set(
+	"n",
+	"ge",
+	function() telescope_builtin.diagnostics({severity=vim.diagnostic.severity.ERROR, bufnr=0}) end,
+	{}
+)
+vim.keymap.set(
+	"n",
+	"gE",
+	function() telescope_builtin.diagnostics({severity=vim.diagnostic.severity.ERROR}) end,
+	{}
+)
+vim.keymap.set(
+	"n",
+	"gw",
+	function() telescope_builtin.diagnostics({severity=vim.diagnostic.severity.WARN, bufnr=0}) end,
+	{}
+)
+vim.keymap.set(
+	"n",
+	"gW",
+	function() telescope_builtin.diagnostics({severity=vim.diagnostic.severity.WARN}) end,
+	{}
+)
 vim.keymap.set("n", "<tab><tab>", alternate_files, {})
-vim.keymap.set("n", "gh", telescope_builtin.help_tags, {})
-vim.keymap.set("n", "gm", telescope_builtin.marks, {})
-vim.keymap.set("n", "gc", telescope_builtin.quickfix, {})
 
 -- Customizing vim.ui.intput callback
 require("dressing").setup({
