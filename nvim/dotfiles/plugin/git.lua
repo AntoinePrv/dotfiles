29680e25758af7070c5d3c0dd3fa610d05b8d0f0
AntoinePrv <AntoinePrv@users.noreply.github.com>
2024-04-26 17:09:23 +0200

    Attach gitsign to untracked

--- nvim/dotfiles/plugin/git.lua
+++ nvim/dotfiles/plugin/git.lua
@@ -7,6 +7,7 @@ require("gitsigns").setup({
         changedelete = { text = "╏" },
         untracked = { text = "┇" },
     },
+    attach_to_untracked = true,
     on_attach = function(bufnr)
         local gs = package.loaded.gitsigns
 
