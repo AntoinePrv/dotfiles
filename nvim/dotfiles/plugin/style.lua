e1ad8c9b9ad3a6c4efc91521b83bc71128c34eb8
AntoinePrv <AntoinePrv@users.noreply.github.com>
2024-07-09 20:48:10 +0200

    Fix statusco, git colors

--- nvim/dotfiles/plugin/style.lua
+++ nvim/dotfiles/plugin/style.lua
@@ -81,6 +81,10 @@ vim.api.nvim_create_autocmd(
     "ColorScheme",
     {
         callback=function()
+            vim.api.nvim_set_hl(0, "Added", { fg=base16_gui("0B") })
+            vim.api.nvim_set_hl(0, "Removed", { fg=base16_gui("08") })
+            vim.api.nvim_set_hl(0, "Changed", { fg=base16_gui("0C") })
+
             vim.api.nvim_set_hl(
                  0, "DiagnosticUnderlineError", { sp=base16_gui("08"), undercurl=true }
             )
