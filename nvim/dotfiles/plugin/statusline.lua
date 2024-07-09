877eae03dd8d28ec7c909b5aae41bf0db96c0876
AntoinePrv <AntoinePrv@users.noreply.github.com>
2024-07-09 20:51:12 +0200

    Simplify statusline colors

--- nvim/dotfiles/plugin/statusline.lua
+++ nvim/dotfiles/plugin/statusline.lua
@@ -1,71 +1,3 @@
-local colors_ansi = {
-    black = 0, -- background, is white in light-mode
-    red = 1,
-    green = 2,
-    yellow = 3,
-    blue = 4,
-    magenta = 5,
-    cyan = 6,
-    white = 7, -- foreground, is black in dark-mode
-}
-
-function load_base16_vars(css)
-    local variables = {}
-
-    -- Iterate over each line in the string
-    for line in css:gmatch("[^\r\n]+") do
-        -- Capture variable name and value
-        local name, value = line:match("%s*%-%-(%w+):%s*(#%x+);")
-        if name and value then
-            variables[name] = value
-        end
-    end
-
-    return variables
-end
-
-local function tinty_path()
-	return os.getenv("USER_TINTED_THEMING_DIR") .. "/css-output-css-variables-file.css"
-end
-
--- Return the content from a string or return some default content
-local function read_base16_file()
-	local file = io.open(tinty_path(), "r")
-    local content = file:read("*a")
-    file:close()
-    return load_base16_vars(content)
-end
-
--- FIXME this is is not reloaded, we need to use highlight groups
-local colors = read_base16_file()
-
-local theme = {
-    normal = {
-        a = { bg = colors.base0D, fg = colors.base06, gui = "bold" },
-        b = { bg = colors.base03, fg = colors.base06 },
-        c = { bg = colors.base02, fg = colors.base06 },
-    },
-    insert = {
-        a = { bg = colors.base0B, fg = colors.base06, gui = "bold" },
-    },
-    visual = {
-        a = { bg = colors.base0E, fg = colors.base06, gui = "bold" },
-    },
-    replace = {
-        a = { bg = colors.base08, fg = colors.base06, gui = "bold" },
-    },
-    command = {
-        a = { bg = colors.base03, fg = colors.base06, gui = "bold" },
-        b = { bg = colors.base02, fg = colors.base06 },
-        c = { bg = colors.base01, fg = colors.base06 },
-    },
-    inactive = {
-        a = { bg = colors.base03, fg = colors.base06, gui = "bold" },
-        b = { bg = colors.base02, fg = colors.base06 },
-        c = { bg = colors.base01, fg = colors.base06 },
-    },
-}
-
 local symbols = {
     file_modified = "",
     file_readonly = "",
@@ -90,15 +22,9 @@ local lualine_sections = {
                 newfile = symbols.file_newfile,
             },
         },
-        {
-            "diff",
-            diff_color = {
-                added = { fg = colors.base0B },
-                modified = { fg = colors.base0E },
-                removed = { fg = colors.base08 },
-            },
-        },
+        { "diff" },
     },
+
     lualine_c = { "encoding", "fileformat", "filetype" },
     lualine_x = {},
     lualine_y = {
@@ -111,12 +37,6 @@ local lualine_sections = {
                 info = symbols.info,
                 hint = symbols.hint,
             },
-            diagnostics_color = {
-                error = { fg = colors.base08 },
-                warn = { fg = colors.base0A },
-                info = { fg = colors.base0D },
-                hint = { fg = colors.base0C },
-            },
             colored = true,
             update_in_insert = false,
             always_visible = false,
@@ -128,7 +48,6 @@ local lualine_sections = {
 require("lualine").setup({
     options = {
         icons_enabled = true,
-        theme = theme,
         component_separators = { left = "", right = "" },
         section_separators = { left = "", right = "" },
     },
