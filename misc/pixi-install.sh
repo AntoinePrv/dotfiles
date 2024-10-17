a8581d83c30eaa443f2f81e08f01982824573b50
AntoinePrv <AntoinePrv@users.noreply.github.com>
2024-10-17 14:40:18 +0200

    new pixi global

---
+++ misc/pixi-install.sh
@@ -0,0 +1,22 @@
+pixi global install --environment devutils \
+  bat \
+  curl \
+  fd-find \
+  fzf \
+  gh \
+  git \
+  htop \
+  jq \
+  nvim \
+  ripgrep \
+  starship \
+  tmux \
+  tmuxp \
+  tree \
+  wget
+
+pixi global install --environment datascience \
+  python \
+  jupyter \
+  polars \
+  pandas \
