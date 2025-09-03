bea76cffb8298ec6d3769c8a2d4259f3b72ae02a
AntoinePrv <AntoinePrv@users.noreply.github.com>
2025-09-03 10:53:17 +0200

    Add cpy/pst aliases

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -5,9 +5,12 @@ elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
 	alias ls='ls -h --color=auto'
 	alias ll='ls -lh --color=auto'
 fi
+alias cpy="clipboard copy"
+alias pst="clipboard paste"
 alias grep='grep --color=auto'
 type bat &> /dev/null && alias cat='bat'
 type nvim &> /dev/null && alias vim='nvim'
+alias git-tree='rg --files | tree --fromfile'
 
 alias tinty='tinty -d "${USER_TINTED_THEMING_DIR}" -c "${XDG_CONFIG_HOME}/misc/tinty.toml"'
 
